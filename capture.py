a1b0d306e37b8df7c6c4ad65a4710815d764e512
amitelle1337 <amitelle1337@gmail.com>
2020-02-09 23:50:17 +0200

    Fixed capture.py, can now show color image and depth image

--- capture.py
+++ capture.py
@@ -7,53 +7,46 @@ import numpy as np
 import cv2
 
 
-def main():
-    pipe = None
-    try:
-        pipe = rs.pipeline()
-        config = rs.config()
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-
-        pipe.start(config)
-
-        capture(pipe)
-
-    finally:
-        pipe.stop()
-
-
-def capture(pipe: rs.pipeline):
+def capture(pipe: rs.pipeline) -> None:
     """
 
     :param pipe: a running pipeline
-    :return: a numpy array of one depth frame.
+    :return: None
     """
-    print(pipe.get_active_profile())
 
-    frames = pipe.wait_for_frames()
-    depth_frame = frames.get_depth_frame()
-    color_frame = frames.get_color_frame()
+    window_name = 'RealSense'
+    cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)  # Init a window
 
-    if not depth_frame or not color_frame:
-        print("---problem---")
-        return None
+    while cv2.waitKey(1) < 0:
 
-    # Convert image to numpy arrays
-    depth_image = np.asanyarray(depth_frame.get_data())
-    color_image = np.asanyarray(color_frame.get_data())
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipe.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            continue
 
-    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
 
-    images = np.hstack((color_image, depth_colormap))
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.1), cv2.COLORMAP_JET)
 
-    # Show images
-    cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('RealSense', images)
-    x = input("stop?")
-    print(x)
+        # Stack both images horizontally
+        images = np.hstack((color_image, depth_colormap))
+
+        # Show images
+        cv2.imshow(window_name, images)
 
 
 if __name__ == "__main__":
-    main()
+    pipe = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)  # Format the depth channel from the camera
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)  # Format the color channel from the camera
+    try:
+        pipe.start(config)
+        capture(pipe)
+    finally:
+        pipe.stop()
