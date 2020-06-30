fa8d9cc019952d08ee07e655ced7d9af6aa47a5b
amitelle1337 <amitelle1337@gmail.com>
2020-06-30 21:10:39 +0300

    Updates after the discord meeting about the calibration.

--- amit/tmp.py
+++ amit/tmp.py
@@ -1,7 +1,21 @@
+from typing import Tuple
+
 import numpy as np
 import pyrealsense2 as rs2
 
 from amit import clean_up, adjustments
+
+
+def write_pc_to_file(frame: np.ndarray, adj: Tuple[float, float, float], intrinsics: rs2.intrinsics,
+                     filename='test.xyz') -> None:
+    assert len(frame.shape) == 2
+
+    with open(filename, 'w') as f:
+        for i in range(frame.shape[0]):
+            for j in range(frame.shape[1]):
+                x, y, z = rs2.rs2_deproject_pixel_to_point(intrinsics, [j, i], frame[i][j])
+                f.write('{} {} {}\n'.format(x + adj[0], y + adj[1], z + adj[2]))
+
 
 pipe = rs2.pipeline()
 try:
@@ -15,7 +29,7 @@ config = rs2.config()
 config.enable_stream(rs2.stream.depth)
 frames = clean_up.capture_frames(config, 10, 30)
 
-th_filter = clean_up.get_filter('threshold_filter', *(0.19, 0.21))
+th_filter = clean_up.get_filter('threshold_filter', *(0.18, 0.22))
 temp_filter = clean_up.get_filter('temporal_filter')
 hole_filter = clean_up.get_filter('hole_filling_filter')
 
@@ -26,3 +40,7 @@ adj = adjustments.calculate_pc_adjustments_by_frames(og_frame, mat, intrinsics)
 
 print(adj)
 
+num = 1
+write_pc_to_file(mat, (0, 0, 0), intrinsics, f'og{num}.xyz')
+write_pc_to_file(mat, adj, intrinsics, f'scanned{num}.xyz')
+write_pc_to_file(og_frame, (0, 0, 0), intrinsics, f'gen{num}.xyz')
